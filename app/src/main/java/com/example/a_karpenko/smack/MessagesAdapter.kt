eb4c83206e2ef7ace2a03ec38286dbb9a6188232
a_karpenko <kapraton@gmail.com>
2017-11-13 13:33:56 -0500

    Remove toolbar from login screen

---
+++ app/src/main/java/com/example/a_karpenko/smack/MessagesAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.a_karpenko.smack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import butterknife.BindView
+import com.example.a_karpenko.smack.models.Chat
+import com.firebase.ui.firestore.FirestoreArray
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.common.collect.Ordering
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import java.util.*
+import java.util.zip.Inflater
+
+var collectionRef : CollectionReference? = FirebaseFirestore.getInstance().collection("chats")
+
+var query: Query? = collectionRef
+        ?.orderBy("timeStamp")
+        ?.limit(50)
+
+val options: FirestoreRecyclerOptions<Chat>? = FirestoreRecyclerOptions.Builder<Chat>()
+        .setQuery(query, Chat::class.java)
+        .build()
+
+
+open class MessagesAdapter : FirestoreRecyclerAdapter<Chat, viewHolder>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): viewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.message_sent, parent, false)
+        return object : viewHolder(v) {}
+    }
+
+    override fun onBindViewHolder(holder: viewHolder?, position: Int, model: Chat?) {
+
+        holder?.messageSent?.text = model?.message
+        holder?.messageSentTime?.text = model?.timeStamp?.toString()
+    }
+
+}
+
+open class viewHolder(view: View?) : RecyclerView.ViewHolder(view) {
+
+    var messageSent : TextView? = view?.findViewById(R.id.messageSentText)
+    var messageReceived : TextView? = view?.findViewById(R.id.messageReceivedText)
+    var messageSentTime : TextView? = view?.findViewById(R.id.messageSentTime)
+    var messageReceivedTime : TextView? = view?.findViewById(R.id.messageReceivedTime)
+
+
+}

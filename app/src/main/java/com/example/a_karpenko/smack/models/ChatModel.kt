8f2e9387e5b57fce6bfcecabe268ccdc79b64039
a_karpenko <kapraton@gmail.com>
2017-11-13 13:34:55 -0500

    Remove toolbar from login screen

---
+++ app/src/main/java/com/example/a_karpenko/smack/models/ChatModel.kt
@@ -0,0 +1,24 @@
+
+package com.example.a_karpenko.smack.models
+
+import com.firebase.ui.firestore.FirestoreArray
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
+
+open class Chat {
+
+    var uid: String = ""
+    var name: String = ""
+    var message: String = ""
+    @ServerTimestamp
+    var timeStamp: Date? = null
+
+    constructor() {} // Needed for Firebase
+    constructor(name:String, message:String, uid:String, timeStamp: Date?) {
+        this.uid = uid
+        this.name = name
+        this.message = message
+        this.timeStamp = timeStamp
+    }
+
+}
